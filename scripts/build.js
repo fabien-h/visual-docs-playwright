0bf8e80caab97c3fdf02dfbbb7246a1cc9cb203d
fabien huet <fabien.huet@gmail.com>
2025-01-25 23:05:03 +0100

    fix: remove dead code

--- scripts/build.js
+++ scripts/build.js
@@ -13,7 +13,3 @@ const packageJson = JSON.parse(
 const versionContent = `export const VERSION = '${packageJson.version}';`;
 fs.writeFileSync(path.join(__dirname, '../dist/src/utils/version.js'), versionContent);
 
-// Copy the styles
-fs.copyFileSync(path.join(__dirname, '../src/html-fragments/styles_content.css'), path.join(__dirname, '../dist/src/html-fragments/styles_content.css'));
-fs.copyFileSync(path.join(__dirname, '../src/html-fragments/styles_document.css'), path.join(__dirname, '../dist/src/html-fragments/styles_document.css'));
-fs.copyFileSync(path.join(__dirname, '../src/html-fragments/styles_navigation.css'), path.join(__dirname, '../dist/src/html-fragments/styles_navigation.css'));
